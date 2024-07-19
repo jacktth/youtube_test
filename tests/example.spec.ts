7a1f19f18854e8a76c7944ec475c3e48dbbc4ea3
jack tang <jack.tangth@gmail.com>
2024-07-19 14:37:57 +0800

    add test for explore links

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -10,13 +10,46 @@ test.describe("home page", () => {
 
     await expect(page).toHaveTitle(/YouTube/);
   });
+  test("has explore links", async ({ page }) => {
+    const elementSelectors = [
+      '[title="Trending"]',
+      '[title="Music"]',
+      '[title="Movies"]',
+      '[title="Live"]',
+      '[title="Gaming"]',
+      '[title="Sports"]',
+      '[title="Fashion & Beauty"]',
+      '[title="Podcastss"]',
+    ];
+    const trendingButton = await page.locator('[title="Trending"]');
+      if ((await trendingButton.count()) === 0) {
+        await page.locator("#guide-button").getByLabel("Guide").click();
+      }
+      for (const selector of elementSelectors) {
+        const locator = page.locator( selector);
+
+        // Wait for the element to be visible (with a timeout)
+        await expect(locator).toBeVisible({ timeout: 5000 });
+
+        // Additional checks if needed
+        await expect(locator).toHaveCount(1);
+      }
+
+  });
 
   test("to trending", async ({ page }) => {
-    await page.locator('#guide-button').getByLabel('Guide').click()
-    await page.locator('[title="Trending"]').click();
-    const currentUrl = await page.url();
-    expect(currentUrl.includes("https://www.youtube.com/feed/trending"));
-  
+    try {
+      const trendingButton = await page.locator('[title="Trending"]');
+      if ((await trendingButton.count()) === 0) {
+        await page.locator("#guide-button").getByLabel("Guide").click();
+      }
+
+      await trendingButton.click();
+      const currentUrl = await page.url();
+      expect(currentUrl.includes("https://www.youtube.com/feed/trending"));
+    } catch (error) {
+      console.error(`An error occurred: ${error.message}`);
+    }
   });
 });
 
@@ -28,26 +61,23 @@ test.describe("trending page", () => {
   test("has correct url", async ({ page }) => {
     const currentUrl = await page.url();
     expect(currentUrl.includes("https://www.youtube.com/feed/trending"));
-
   });
 
   test("has tabs", async ({ page }) => {
-    await expect(page.locator('[tab-title=Now]')).toBeVisible();
-    await expect(page.locator('[tab-title=Music]')).toBeVisible();
-    await expect(page.locator('[tab-title=Gaming]')).toBeVisible();
-    await expect(page.locator('[tab-title=Movies]')).toBeVisible();
- 
+    await expect(page.locator("[tab-title=Now]")).toBeVisible();
+    await expect(page.locator("[tab-title=Music]")).toBeVisible();
+    await expect(page.locator("[tab-title=Gaming]")).toBeVisible();
+    await expect(page.locator("[tab-title=Movies]")).toBeVisible();
   });
 
-  test("has videos", async ({ page },testInfo) => {
-    await expect(page.locator('ytd-video-renderer').first()).toBeVisible();
+  test("has videos", async ({ page }, testInfo) => {
+    await expect(page.locator("ytd-video-renderer").first()).toBeVisible();
     const screenshot = await page.screenshot();
 
     // Attach the screenshot to the report
-    await testInfo.attach('Example Screenshot', {
-        body: screenshot,
-        contentType: 'image/png',
+    await testInfo.attach("Example Screenshot", {
+      body: screenshot,
+      contentType: "image/png",
     });
-
   });
 });
